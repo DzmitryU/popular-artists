5114de9f03f6ae4763a42a73b297ebde00c8c65f
Dzmitry Ulezla <dzmitry.ulezla@globant.com>
2019-09-15 18:55:02 +0300

    [GH-13] feat: Added fastify and static assets

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 import { ArtistsModule } from './artists/artists.module';
 
 @Module({
   imports: [
     ArtistsModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'docs'),
-    }),
   ],
 })
 export class AppModule {}
