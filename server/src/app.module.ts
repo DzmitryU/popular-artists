8c23fc5c3bed57fcbefa881f770e4862554eb2fa
Dzmitry Ulezla <dzmitry.ulezla@globant.com>
2019-09-15 18:39:08 +0300

    [GH-13] feat: Added server static module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { ArtistsModule } from './artists/artists.module';
 
 @Module({
-  imports: [ArtistsModule],
+  imports: [
+    ArtistsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'docs'),
+    }),
+  ],
 })
 export class AppModule {}
